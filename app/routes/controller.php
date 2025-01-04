ae3d8fc59d0faa6ecd9dde8b7e3fd5760d7b30ee
Tabs Macedo <tabata.macedo1897@gmail.com>
2025-01-04 16:50:47 -0300

    :wastebasket: remove: Excluindo a pasta 'route' não utilizada

--- app/routes/controller.php
+++
@@ -1,5 +0,0 @@
-<?php
-header('Content-Type: application/json');
-
-// Teste simples para confirmar que o fetch está chegando aqui
-echo json_encode(["status" => "success", "message" => "Chegou no routes/controller.php"]);
