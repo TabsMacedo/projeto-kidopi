12173f8b23c0b2b535967a024cc5ade3258e5bde
Tabs Macedo <tabata.macedo1897@gmail.com>
2025-01-06 09:25:58 -0300

    :sparkles: feat: Criada rota /api/covid para redirecionar requisições ao controller

--- index.php
+++ index.php
@@ -1,13 +1,27 @@
-<!-- O Index.php vai fazer o redirecionamento para home, e para api controller -->
-<!-- carregar os arquivos que preciso conectar antes -->
 <?php
 require_once './app/controllers/HomeController.php';
+require_once './app/controllers/CovidController.php';
 
 $requestUri = $_SERVER['REQUEST_URI'];
 
 if ($requestUri === '/' || $requestUri === '') {
     $controller = new HomeController();
     $controller->index();
+
+} elseif (strpos($requestUri, '/api/covid') === 0) {
+    $controller = new CovidController();
+
+    if (isset($_GET['lastAccess']) && $_GET['lastAccess'] === 'true') {
+        $controller->getLastAccess();
+    } elseif (isset($_GET['pais'])) {
+        $country = $_GET['pais'];
+        $controller->getCovidData($country);
+    } else {
+        http_response_code(400);
+        echo json_encode(['error' => 'Parâmetros inválidos ou ausentes.']);
+    }
+
 } else {
+    http_response_code(404);
     echo "Recurso não encontrado!";
 }
