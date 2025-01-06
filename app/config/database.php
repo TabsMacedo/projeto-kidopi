d51428c76d128d3f93e61b300645e02e5f6197c0
Tabs Macedo <tabata.macedo1897@gmail.com>
2025-01-06 09:12:56 -0300

    Alterada a configuração de conexão com o banco de dados de MySQL simples para PDO

--- app/config/database.php
+++ app/config/database.php
@@ -1,15 +1,19 @@
-<!-- configurações para conectar com um banco de dados -->
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "senha";
 
-// Create connection
-$conn = new mysqli($servername, $username, $password);
+$host = 'localhost';
+$dbname = 'covid19';
+$username = 'root';
+$password = 'senha';
+$charset = 'utf8mb4';
 
-// Check connection
-if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
+$dsn = "mysql:host=$host;dbname=$dbname;charset=$charset";
+
+try {
+
+    $conn = new PDO($dsn, $username, $password);
+
+    
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    echo "Erro ao conectar: " . $e->getMessage();
 }
-echo "Connected successfully";
-?>
