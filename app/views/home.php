820f473ab5a4b117e1df053fdd8582b9c48956c9
Tabs Macedo <tabata.macedo1897@gmail.com>
2025-01-10 14:12:59 -0300

    :lipstick: fix(css): Correções de estilo no CSS

--- app/views/home.php
+++ app/views/home.php
@@ -4,6 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="/public/img/faviconkidopi.ico" type="image/x-icon">
     <title>Painel Informativo - Covid</title>
     <link rel="stylesheet" href="/public/css/reset.css">
     <link rel="stylesheet" href="/public/css/style.css">
